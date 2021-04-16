1e2abae08bba1c292f2c6621a415c3768440b2fd
Cerebreo <nellirism@outlook.com>
2021-04-15 23:47:37 -0700

    refactoring code  and adding data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,6 +234,31 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+
+
+
+var loadTasks = function() {
+    // Get the tasks items from localStorage
+
+    // Convert tasks from the string format back into an array of objects
+
+    // Iterate through a tasks array and create task elements on the page from it
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (tasks === null) {
+        return false;
+    }
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the 'createTaskEl()' function
+        createTaskEl(savedTasks[i]);
+    }
+}
+
+
+loadTasks()
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
